c270e2874999dbc82eefdcc41a7995e4c6767bf5
yunjae228 <yvkqzhs7@gmail.com>
2021-08-03 17:43:58 +0900

    js 구조 설정

---
+++ js/book.js
@@ -0,0 +1,10 @@
+async function main() {
+    //버튼에 이벤트 연결
+    //브라우저에서 id 가져오기
+    //토큰 체크
+    //토큰으로 서버에서 나의 정보 가져오기
+    //책을 서버에서 받아오기
+    //받아온 책을 그리기
+}
+
+document.addEventListener('DOMContentLoaded', main);
